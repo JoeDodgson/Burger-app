a24d127c57e3f96fdfb47ae905e234d970263f6a
Joe Dodgson <Joe.Dodgson@arup.com>
2020-06-23 13:55:43 +0100

    Created seeds file for burger table

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+USE burgers_db;
+INSERT INTO burgers(burger_name, devoured)
+VALUES
+	("Beef burger", true),
+	("Cheese burger", true),
+    ("Chicken burger", true),
+    ("Bean burger", false),
+    ("Soy burger", false),
+	("Kangaroo burger", false);
