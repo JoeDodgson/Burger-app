b4ccdfe83a3bcefbc3f9a3c77f4033e77cbfae6a
Joe Dodgson <Joe.Dodgson@arup.com>
2020-06-24 09:33:05 +0100

    Structured the controller file, including requiring in modules ad files and exporting the router

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,17 @@
+// Require in modules
+const express = require("express");
+
+// Create the router variable
+const router = express.Router();
+
+// Import the model (burger.js)
+const burger = require("../models/burger.js");
+
+// Routes (Get)
+
+// Post
+
+// Put
+
+// Export routes for server.js to use.
+module.exports = router;
