90d0c57b7ca1927290594ccbd8bdeb3b978417ea
Joe Dodgson <Joe.Dodgson@arup.com>
2020-06-24 15:00:41 +0100

    Created handlers.js file to handle the button click event listening. Linked this file to the main.handlebars and linked to jQuery

---
+++ public/assets/js/handlers.js
@@ -0,0 +1,9 @@
+// Wait until the DOM is fully loaded Make sure we wait to attach our handlers .
+$(function() {
+
+  // Listener for the form submit
+
+
+  // Listener for the devoured button click
+
+});
