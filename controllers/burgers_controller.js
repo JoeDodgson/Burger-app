e5a7b095cbe3e689e1e9e01246df1902ed561a30
Joe Dodgson <Joe.Dodgson@arup.com>
2020-06-24 09:40:25 +0100

    Set up the single get request

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -7,7 +7,19 @@ const router = express.Router();
 // Import the model (burger.js)
 const burger = require("../models/burger.js");
 
-// Routes (Get)
+// Create routes (only 1 route for this application)
+router.get("/", (req, res) => {
+
+    // Call the 'all' method from the model
+    burger.all(data => {
+
+        // Create a handlebars object using the data returned
+        const hbsObj = { burgers: data };
+
+        // Use the handlebars object to render the page
+        res.render("index", hbsObj);
+    });
+});
 
 // Post
 
