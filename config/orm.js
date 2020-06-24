c79b3d09c51d56169f209d93c8d72524fbd98209
Joe Dodgson <Joe.Dodgson@arup.com>
2020-06-24 08:31:21 +0100

    Created helper functions for creating SQL query strings in orm.js

--- config/orm.js
+++ config/orm.js
@@ -1,6 +1,45 @@
 // Import MySQL connection.
 const connection = require("connection.js");
 
+// Returns a string of a given number of question marks separated by commas ("?,?,?") for use in SQL syntax
+const questionMarksString = num => {
+    let str = "";
+
+    if (num > 0) {
+        str += "?";
+    }
+
+    for (let i = 1; i < num; i++) {
+        str += ",?";
+    }
+
+  return str;
+}
+
+// Converts object key value pairs into string for use in SQL syntax
+const objToSqlString = obj => {
+  const arr = [];
+
+  // Loop through each key in obj and push the key value pair as a string int arr
+  for (let key in obj) {
+
+    let value = obj[key];
+
+    // Check .hasOwnProperty of Object to skip hidden properties
+    if (Object.hasOwnProperty.call(obj, key)) {
+      // If the value is a string with spaces, add quotations round it
+      if (typeof value === "string" && value.indexOf(" ") !== -1) {
+        value = `'${value}'`;
+      }
+      // Push into arr in the form "key=value"
+      arr.push(`${key}=${value}`);
+    }
+  }
+
+  // Return a single comma-separated string of SQL key-value pairs
+  return arr.toString();
+}
+
 // ORM object containing all SQL query functions
 const orm = {
 
