98fd3997348101e3c96d882e0347a49ba55efe64
Joe Dodgson <Joe.Dodgson@arup.com>
2020-06-24 09:21:20 +0100

    Created the burger object in burger.js which has methods that call the ORM function, specifying the burger table in the database

--- models/burger.js
+++ models/burger.js
@@ -1,9 +1,23 @@
 // Require app files
 const orm = require("../config/orm.js");
 
-// Create the code that will call the ORM functions using burger specific input for the ORM
+// The burger object has methods that call the ORM functions, specifying the burger table
 const burger = {
-
+    all: cb => {
+        orm.all("burgers", res => {
+            cb(res);
+        });
+    },
+    create: (cols, vals, cb) => {
+        orm.create("burgers", cols, vals, res => {
+            cb(res);
+        });
+    },
+    devour: (objColVals, condition, cb) => {
+        orm.update("burgers", objColVals, condition, res => {
+            cb(res);
+        });
+    }
 };
 
 // Export at the end of the `burger.js` file.
