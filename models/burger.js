9cadfb113b9212e532be6ece1aa9b4d1add69ece
Joe Dodgson <Joe.Dodgson@arup.com>
2020-06-24 08:27:21 +0100

    Setup structure of orm.js and burgers.js files

---
+++ models/burger.js
@@ -0,0 +1,10 @@
+// Require app files
+const orm = require("../config/orm.js");
+
+// Create the code that will call the ORM functions using burger specific input for the ORM
+const burger = {
+
+};
+
+// Export at the end of the `burger.js` file.
+module.exports = burger;
