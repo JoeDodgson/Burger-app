ae629c08c055861b78f5fb22a6a7cc8c63a40c64
Joe Dodgson <Joe.Dodgson@arup.com>
2020-06-24 13:34:43 +0100

    Set handlebars as the template engine

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 // Node modules
-var express = require("express");
+const express = require("express");
+const exphbs = require("express-handlebars");
 
 // Declare the Express app and port (use process.env port definition, ready for deployment to Heroku)
-var app = express();
-var PORT = process.env.PORT || 8080;
+const app = express();
+const PORT = process.env.PORT || 8080;
 
 // Set up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -15,6 +16,10 @@ app.use(express.static("public"));
 // Link the server to the controller file for routing and handling requests
 const routes = require("./controllers/burgers_controller.js");
 app.use(routes);
+
+// Set Handlebars as the template engine
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 // Start the server and begin listening
 app.listen(PORT, function() {
