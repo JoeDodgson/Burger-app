11dbc4ce77ab169f74356a8cb8b18830150a85e2
Joe Dodgson <Joe.Dodgson@arup.com>
2020-06-24 08:31:04 +0100

    Removed orm.js from the .gitignore

---
+++ config/orm.js
@@ -0,0 +1,10 @@
+// Import MySQL connection.
+const connection = require("connection.js");
+
+// ORM object containing all SQL query functions
+const orm = {
+
+};
+
+// Export the ORM object for use in burger.js (the model)
+module.exports = orm;
